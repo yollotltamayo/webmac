0348f76577b08a641c853eb8bcd97129f34189b8
yollotltamayo <yollotltamayo@comunidad.unam.mx>
2021-03-19 17:52:40 -0600

    routing para los archivos y agrego html y css

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,19 @@
 
 #[macro_use]
 extern crate rocket;
-
+use rocket::response::NamedFile;
+use rocket_contrib::serve::StaticFiles;
+use std::path::{Path, PathBuf};
 #[get("/")]
-fn hello() -> String {
-    format!("¡Hola MAC!")
+fn index() -> Option<NamedFile> {
+    NamedFile::open("static/index.html").ok()
 }
-
+#[get("/<file..>")]
+fn files(file:PathBuf)->Option<NamedFile> {
+    NamedFile::open(Path::new("static").join(file)).ok()
+}
 fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+    rocket::ignite()
+        .mount("/" , routes![index,files])
+        .launch();
 }
