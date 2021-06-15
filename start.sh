3073bc2fd6f9acd25c04930dfbeb93501a323e04
yollotltamayo <yollotltamayo@comunidad.unam.mx>
2021-06-15 14:14:46 -0500

    cambia start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@ export FLASK_APP=main.py
 if  ! [ -x "$(command -v nodemon)" ]; then
     nodemon --exec "flask run" -e css,js,html
 else
-    echo "try the following npm i -g nodemon"
+    echo "prueb haciendo npm i -g nodemon"
     flask run
 fi
