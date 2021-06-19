c1e6e70d2c36c6e7c9d3cc0d85338bfa7e47b2af
yollotltamayo <yollotltamayo@comunidad.unam.mx>
2021-06-19 00:28:12 -0500

    agrega backend para la base de datos

---
+++ db.py
@@ -0,0 +1,36 @@
+import sqlite3
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+@with_appcontext
+def init_db():
+    db = get_db()
+
+    users = """
+    create table if not exists user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL); """
+    db.execute(users)  
+    db.commit()
+
+
+
