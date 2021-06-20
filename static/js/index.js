3321e1dc31682cfe00b041fd157725a5b753cded
Bruno A. Muciño <mucinoab@gmail.com>
2021-06-20 15:13:05 -0500

    Salva pantallas (animación en canvas)
    Y pasa a TS el JS.

--- static/js/index.js
+++ static/js/index.js
@@ -1,30 +1,26 @@
-const liElement = (data) => {
-    const div = document.createElement('div');
-    const h3 = document.createElement('h3');
-    const h2 = document.createElement('h2');
-    const bordes = document.createElement('h1');
+function liElement(data) {
     const h1 = document.createElement('h1');
-    div.className= "card";
-    bordes.className="bordes";
-    h3.innerText="topico";
-    h2.innerText=data.title;
-    h1.innerText=data.description;
+    const h2 = document.createElement('h2');
+    const h3 = document.createElement('h3');
+    const div = document.createElement('div');
+    const bordes = document.createElement('h1');
+    h1.innerText = data.description;
+    h2.innerText = data.title;
+    h3.innerText = "topico";
+    div.className = "card";
+    bordes.className = "bordes";
     div.appendChild(h3);
     div.appendChild(h2);
     div.appendChild(bordes);
     div.appendChild(h1);
-    //const div2 = document.createElement('div');
-    //div2.className= "card";
-    //div2.appendChild(div);
     return div;
-};
-const loadDom = (data) => {
-    const ul = document.createElement('ul');
+}
+;
+function loadDom(data) {
     const main = document.getElementById('content');
     data.map((user) => main.appendChild(liElement(user)));
-};
-var res =  fetch('https://ghibliapi.herokuapp.com/films')
-    .then( res => res.json() )
-    .then(data => {
-        loadDom(data);
-    });
+}
+;
+fetch('https://ghibliapi.herokuapp.com/films')
+    .then(res => res.json())
+    .then(data => loadDom(data));
