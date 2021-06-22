e03dca82aff0bfff39c207970a545f3d45b09e64
yollotltamayo <yollotltamayo@comunidad.unam.mx>
2021-06-22 11:58:32 -0500

    agrega cambios a las cartas

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export FLASK_APP=main.py
 if   [ -x "$(command -v nodemon)" ]; then
-    nodemon --exec "flask run" -e css,js,html,py
+    nodemon --exec "flask run" -e css,js,html,py,ts
 else
     echo "prueb haciendo npm i -g nodemon"
     flask run
