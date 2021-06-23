81fc7c2bdf7d15d327ba538dc5e8e9870048a669
yollotltamayo <yollotltamayo@comunidad.unam.mx>
2021-06-23 10:59:41 -0500

    agrega panel de usuario

--- main.py
+++ main.py
@@ -17,14 +17,16 @@ PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 DATABASE = 'base.sqlite'
 
 app.config['DATABASE'] = DATABASE
-
-def validar(user, password):
+def get_user(user):
     db = d.get_db()
 
     _user = db.execute(
         'SELECT * FROM user WHERE username = ?', (user,)
     ).fetchone()
+    return _user
 
+def validar(user, password):
+    _user= get_user(user)
     if _user is None:
         return False
     if _user['password'] == hash_password(user, password):
@@ -46,13 +48,8 @@ def login():
         if  validar(user, password):
             session.clear()
             session['username'] = request.form['username']
-            session['avatar'] = "lol"
-            db = d.get_db()
-            avatar = db.execute( """SELECT avatar
-                FROM user
-                WHERE username = ?;""", (user,)
-            ).fetchone() 
-            session['avatar'] = "lol" if avatar is None else avatar['avatar']
+            _user =  get_user(user)
+            session['avatar'] = "lol" if _user is None else _user['avatar']
             return redirect('/')
         flash('Usuario o contraseña incorrectos')
         return redirect('/login')
@@ -66,11 +63,7 @@ def register():
         avatar = request.form['avatar']
 
         db = d.get_db()
-        if db.execute(
-            """SELECT ID
-            FROM user
-            WHERE username = ?""", (user,)
-        ).fetchone() is not None:
+        if get_user(user) is not None:
             flash("Usuario ya registrado.")
             return redirect('/register')
 
@@ -87,17 +80,77 @@ def register():
 def panel():
     if 'username' in session:
         if request.method == 'POST':
-                pass
-        return render_template('panel.html')
+            new_pass = request.form['newpassword']
+            password = request.form['password']
+            avatar = request.form['avatar']
+
+            user = session['username']
+            new_user = request.form['username']
+
+            error = None
+
+            if (password,new_pass) == ("",""):
+                password = None
+            elif (len(password) >0 , len(new_pass) > 0 ) == (True,True):
+                if validar(user,password) :
+                    password = new_pass
+                else:
+                    error= 'Contraseña incorrecta.' 
+            else:
+                error = "Ambas contraseñas deben ser llenadas."
+
+            if new_user != user and get_user(new_user) is not None :
+                error = 'El nuevo username no esta disponible.'
+
+            _user = get_user(user)
+            def query ( new_word = None ):
+                query  = """ UPDATE user
+                    SET username = ?,
+                        avatar = ?"""
+                if new_word:
+                    query += ",password = ? "
+                query += """WHERE id = ?;"""
+                return query
+
+            if error:
+               flash(error)
+               return render_template('panel.html',user=session['username'],avatar=session['avatar'])
+            else:
+               db = d.get_db()
+               if password:
+                   db.execute( query(password) ,
+                           (new_user,avatar,hash_password(new_user, new_pass),_user['id']))
+                   db.commit()
+               else:
+                   print(query())
+                   db.execute(query(), (new_user, avatar,_user['id']))
+                   db.commit()
+               session['username'] = new_user
+               session['avatar'] = avatar
+               return redirect('/login')
+        return render_template('panel.html',user=session['username'],avatar=session['avatar'])
+
+
+
     return redirect('/login')
 
 @app.route('/logout')
 def logout():
     session.clear()
     return redirect('/login')
+
 @app.route('/pixel')
 def pixel():
     return render_template("pixel.html")
+
+@app.route('/delete',methods=["POST"])
+def delete():
+    db = d.get_db()
+    print(session['username'])
+    db.execute("""DELETE FROM user WHERE username = ?;""",(session['username'],))
+    db.commit()
+    session.clear()
+    return redirect("/")
 
 if __name__ == '__main__':
     d.init_db()
